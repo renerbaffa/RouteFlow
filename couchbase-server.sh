47fadfe4e8db9da3e384a98577d02b2f0b9ece1f
viniciuskol <viniciuskol@yahoo.com.br>
2013-07-06 18:01:52 -0300

    Messages were translated to english

--- couchbase-server.sh
+++ couchbase-server.sh
@@ -7,7 +7,7 @@
 		wget http://packages.couchbase.com/releases/2.0.1/couchbase-server-enterprise_x86_2.0.1.deb
 		dpkg -i http://packages.couchbase.com/releases/2.0.1/couchbase-server-enterprise_x86_2.0.1.deb
 	else
-		echo "Precisa ser root."
+		echo "You must be a root user."
 	fi
 
 exit 0
